1455eeb544143782fa4bdf7626ac1f2b590d80af
bksim <simpavid@gmail.com>
2014-08-28 18:34:49 -0700

    set up basic python API online panda-box.appspot.com for queries

---
+++ README.txt
@@ -0,0 +1,13 @@
+=====
+
+to deploy on google app engine, open git bash and type:
+appcfg.py update <<directory>>
+
+=====
+
+to test locally, open git bash and type:
+dev_appserver.py <<directory>>
+
+and go to: http://localhost:8080/
+
+=====
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import getter
+
+app = Flask(__name__)
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/query', methods=['GET'])
+def result():
+	keywords = str(request.args['keyword'])
+	result = getter.parse_ebay_json(getter.get_from_ebay(keywords))
+	return render_template('result.html', json_result=str(result))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
